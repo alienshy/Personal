bf4dceea6d759dd0e72723814fce686e22e8f893
alienshy <alisahverdiyeW8@gmail.com>
2023-12-20 17:27:57 +0400

    m

---
+++ personal.js
@@ -0,0 +1,29 @@
+let body = document.querySelector("body")
+body.innerHTML+=`
+<nav>
+<div class="con navcon">
+    <h3>Start Bootstrap
+    </h3>
+    <ul>
+        <a href="#">Home</a>
+        <a href="#">Resume</a>
+        <a href="#"> Projects</a>
+        <a href="#">Contact</a>
+    </ul>
+</div>
+</nav>
+<section>
+<button class="desi">DESIGN · DEVELOPMENT · MARKETING</button>
+<h2>I can help your business to</h2>
+<h1 class="h11">Get online and grow fast</h1>
+<div class="divbutton"><button class="button1">Resume</button><button class="button2">Project</button></div>
+<div class="divimg"><img src="https://startbootstrap.github.io/startbootstrap-personal/assets/profile.png" alt=""></div>
+<h1 class="h12">About Me</h1>
+<p class="p1">My name is Start Bootstrap and I help brands grow.</p>
+<p class="p2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur
+    autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem
+</p>
+<div class="idiv"><i class="bi bi-twitter"></i><i class="bi bi-linkedin"></i><i class="bi bi-github"></i></div>
+<div class="res"><p>Copyright © Your Website 2023</p><span><a href="#" class="sd">Privacy </a><a href="#" class="sd">Terms ·</a><a href="#" class="sd">Contact</a></span></div>
+</section>
+`
